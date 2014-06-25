d88ae9cb2b4b4092477f921e3dd33d9374e5bd1c
Sam Fishman <sfishman@college.harvard.edu>
2014-06-25 00:26:08 -0700

    wait for highlighter.js to finish running

--- background.js
+++ background.js
@@ -1,15 +1,18 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
-    chrome.tabs.executeScript({file: "highlighter.js"});
-    chrome.tabs.executeScript({code: "doAction(true)"});
+    chrome.tabs.executeScript({file: "highlighter.js"}, function(results) {        
+        chrome.tabs.executeScript({code: "doAction(true)"});
+    });
 });
 
 chrome.commands.onCommand.addListener(function(command) {
     if (command == "highlight-code") {
-        chrome.tabs.executeScript({file: "highlighter.js"});
-        chrome.tabs.executeScript({code: "doAction(true)"});
+        chrome.tabs.executeScript({file: "highlighter.js"}, function(results) {
+            chrome.tabs.executeScript({code: "doAction(true)"});
+        });
     }
     else if (command == "un-highlight-code") {
-        chrome.tabs.executeScript({file: "highlighter.js"})
-        chrome.tabs.executeScript({code: "doAction(false)"});
+        chrome.tabs.executeScript({file: "highlighter.js"}, function(results) {
+            chrome.tabs.executeScript({code: "doAction(false)"});
+        });
     }
 });
